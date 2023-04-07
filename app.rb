6f3794f9fd189d3a54516df16d835c34cd40352b
Denis Tarasov <denchickuz@yandex.ru>
2023-04-08 01:05:08 +0300

    init database, make @db

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true #эта настройка нужна, чтобы результаты возвращались в виде хэша, а не массива
+end
+
+before do #before выполняется каждый раз перед выполнением любого HTTP-запроса
+	init_db
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
