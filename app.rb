a8eedd5ad9c8edb211bbdff07e25c145d09ecc29
Denis Tarasov <denchickuz@yandex.ru>
2023-04-13 19:03:19 +0300

    new commit

--- app.rb
+++ app.rb
@@ -95,6 +95,19 @@ post '/details/:post_id' do
 	# получаем переменную из post-запроса
 	content = params[:content]
 
+	@db.execute 'insert into Comments
+		 (
+		 	content,
+		 	created_date, 
+		 	post_id
+		 ) 
+		 	values 
+		 (
+		 	?,
+		 	datetime(),
+		 	?
+		 );', [content, post_id]
+
 	erb "You typed comment #{content} for post #{post_id}"
 
 end
